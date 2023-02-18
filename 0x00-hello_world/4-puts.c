cb051ddd412954b464c16e13243320893db9a474
Chukwudum Stanley <stylshstann@gmail.com>
2023-02-18 04:37:44 -0800

    modified

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Return - Always 0 (Success)
+ * main - A program that prints a line with puts function
+ * Return: 0 (approved)
  */
 
 int main(void)
