38eecb1d8ff7e6e2da42e0ce753fad48d9aaa753
Chukwudum Stanley <stylshstann@gmail.com>
2023-02-20 01:03:36 -0800

    modified

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int hexi = 0;
+
+	while (hexi < 16)
+	{
+		if (hexi >= '0' || hexi <= '10')
+			putchar(hexi);
+		else
+		{
+			putchar(hexi + 32);
+		}
+		putchar('\n');
+		return (0);
+	}
+}
