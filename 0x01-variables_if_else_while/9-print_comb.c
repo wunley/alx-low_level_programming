65a512f71ef1c6f1f83778a452b3fbdfac1699f1
Chukwudum Stanley <stylshstann@gmail.com>
2023-02-20 01:27:38 -0800

    modified

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints all single digits of base 10
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int basey;
+
+	for (basey = 0; basey < 10; basey++)
+	{
+		putchar(basey);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
