e8b30608366b30ae43f7256c43227920cdd13dfb
Chukwudum Stanley <stylshstann@gmail.com>
2023-02-19 20:37:38 -0800

    modified

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Displays base 10 numbers
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%i", n);
+		n++;
+	}
+	printf('\n');
+	return (0);
+}
