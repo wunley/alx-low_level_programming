1c4c27b5bc7b57e6e829d599fed95c07b9dc2c18
Chukwudum Stanley <stylshstann@gmail.com>
2023-02-19 21:14:40 -0800

    modified

---
+++ 0x01-variables_if_else_while/7-print_tebhapla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints lowercase alphabets in reverse
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch = 'z';
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
