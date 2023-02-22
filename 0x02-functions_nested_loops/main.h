05f51b522815918f6cc32b0eee263051a4243a8a
Chukwudum Stanley <stylshstann@gmail.com>
2023-02-22 01:21:03 -0800

    modified

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,6 @@
-#ifndef HOLBERTON_H_INCLUDED
-#define HOLBERTON_H_INCLUDED
 int _putchar(char c);
 void print_alphabet(void);
-void print_alphabet_x10(coid);
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
@@ -11,16 +9,17 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void print_to_98(int n);(void);
-int add(int, int);
-void print_to_98(int n);(void);
-int add(int, int);
-void print_to_98(int n);(void);
-int add(int, int);
-void print_to_98(int n);(void);
-int add(int, int);
-void print_to_98(int n);(void);
-int add(int, int);
 void print_to_98(int n);
+void print_times_tables(int n);
 
-#endif
+
+
+
+
+
+
+
+
+
+
+
