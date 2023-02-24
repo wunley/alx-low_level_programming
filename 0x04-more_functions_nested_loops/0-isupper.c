a05b4cc14e58b39a9c54034df4b19a8ca192ac91
Chukwudum Stanley <stylshstann@gmail.com>
2023-02-24 02:01:30 -0800

    modified

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#inlude "main.h"
+/**
+ * _isupper - Check for upper case letter
+ *
+ * @c: character to be checked
+ *
+ * Return: 1 if c is upper case, 0 if otherwise
+ */
+int _isupper(int c)
+{
+	if (c <= 'Z' && c >= 'A')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
