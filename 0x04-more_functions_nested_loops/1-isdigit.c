10902203b7672949252cc1e2cad78681a51823e2
Chukwudum Stanley <stylshstann@gmail.com>
2023-02-24 03:06:49 -0800

    modified

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isdigit - Check for upper case letter
+ *
+ * @c: value to be checked
+ *
+ * Return: 1 if c is a digit, 0 if otherwise
+ */
+int _digit(int c)
+{
+	if (c <= '9' && c >= '0')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
