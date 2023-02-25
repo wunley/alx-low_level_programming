47bb378c27631c7e1d4572d9c1a2fc8b242bbd32
Chukwudum Stanley <stylshstann@gmail.com>
2023-02-24 19:30:59 -0800

    modified

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,14 +13,17 @@ void print_diagonal(int n);
 		for (len = 0; len < n; len++)
 		{
 			for (space = 0; space < len; space++)
+			{
 				_putchar(' ');
 				_putchar('\\');
 
 				if (len == n - 1)
+				{
 					continue;
+				}
 				_putchar('\n');
 			}
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
