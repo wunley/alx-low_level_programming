7f874e96979798da49e518ca47974d5d10dc11ca
Chukwudum Stanley <stylshstann@gmail.com>
2023-02-27 16:10:30 -0800

    modified

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - prints a string followed by a new line to stdout
+ * @str: a parameter
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n');
+}
