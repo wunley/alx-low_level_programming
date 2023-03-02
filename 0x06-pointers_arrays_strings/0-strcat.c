e2591775b35e576e4b16d07cc7a53e3715b82ca6
Chukwudum Stanley <stylshstann@gmail.com>
2023-03-02 06:23:04 -0800

    modified

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -17,7 +17,7 @@ char *_strcat(char *dest, char *src)
 	while (dest[index++])
 		dest_len++;
 
-	for (index = 0; src[index] && index < n; index++)
+	for (index = 0; src[index]; index++)
 	{
 		dest[dest_len++] = src[index];
 
