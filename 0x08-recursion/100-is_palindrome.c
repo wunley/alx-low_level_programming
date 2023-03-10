7c066f1b8983546fc3bd5f5d2ace50a184095033
Chukwudum Stanley <stylshstann@gmail.com>
2023-03-09 18:45:50 -0800

    modified

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -12,7 +12,7 @@ int is_palindrome(char *s)
 {
 	if (*s == 0)
 		return (1);
-	return (check_pals(s, 0, _strlen_recursion(s)));
+	return (check_pal(s, 0, _strlen_recursion(s)));
 }
 /**
  * _strlen_recursion - returns the length of a string
