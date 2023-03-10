a398f38ac641a22d095455c5c4788783856b0777
Chukwudum Stanley <stylshstann@gmail.com>
2023-03-09 18:41:00 -0800

    modified

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-int actual_primt(int n, int i);
+int actual_prime(int n, int i);
 /**
  * is_prime_number - says if an integer is a prime number or not
  * @n: number to evaluate
